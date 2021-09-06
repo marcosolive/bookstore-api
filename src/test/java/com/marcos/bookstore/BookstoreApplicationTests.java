afd954c45c44f5f66e5a10c195f3b7ae7836d0ad
marcosolive <marcosolive@hotmail.com>
2021-09-06 16:10:03 -0300

    primeiro commit

---
+++ src/test/java/com/marcos/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
